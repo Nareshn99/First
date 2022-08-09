e75dd22bd66ec72033d210d5d4d1c77bd5e5fbdb
Nareshn99 <nareshngohil99@gmail.com>
2022-08-09 18:18:42 +0530

    THIS IS *AUG ASSIGNMENT

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,37 +2,35 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr = [1, 2, 3,4, 5, 6, 7,9,10]
+    let m = ((arr.length + 1) * ((arr.length + 1) + 1) / 2);
+    let a = 0;
 
-    res.send('My second ever api!')
+    for (let i = 0; i < arr.length; i++) {
+        a = a + arr[i];
+    }
+
+    let missingNumber = m - a;
+
+    res.send({ "Your Missing Number is ": missingNumber });
+});
+router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr = [33, 34, 35,36, 37, 38,39,41]
+    let m = ((arr.length + 1) * ((arr[0]) + arr[arr.length - 1]) / 2);
+    let a = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        a = a + arr[i];
+    }
+
+    let missingNumber = m - a;
+
+    res.send({ "Your Missing Number is ": missingNumber });
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
 
 module.exports = router;
