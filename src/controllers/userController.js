68803489eda3135723c442fee26659e89a593b4b
Nareshn99 <nareshngohil99@gmail.com>
2022-08-25 10:57:01 +0530

    Assignment 24 Aug

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,21 @@
-const UserModel= require("../models/userModel")
+const UserModel = require("../models/userModel")
 
-
-
-
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+const basicCode1 = async function (req, res) {
+    //console.log("hey man, congrats you have reached the Handler1")
+    res.send({ msg: "This is coming from controller1 (handler1)" })
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+
+const basicCode2 = async function (req, res) {
+   // console.log("hey man, congrats you have reached the Handler2")
+    res.send({ msg: "This is coming from controller2 (handler2)" })
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+const getUsersData = async function (req, res) {
+    let allUsers = await UserModel.find()
+    res.send({ msg: allUsers })
+}
+
+module.exports.getUsersData = getUsersData
+module.exports.basicCode1 = basicCode1
+module.exports.basicCode2 = basicCode2
