cf3a30f12f146d3a3cf23099c62ba999fe2c0626
Nareshn99 <nareshngohil99@gmail.com>
2022-08-29 15:26:49 +0530

    assignment solution of auth-

---
+++ src/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("./models/userModel");
+
+const mw = async function (req, res, next) {
+  let token = req.headers["x-auth-token"];
+  if (!token)
+    return res.send({ status: false, msg: "token must be present" });
+
+  let decodedToken = jwt.verify(token, "secret-key");
+  if (!decodedToken)
+    return res.send({ status: false, msg: "token is invalid" });
+
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  if (!user)
+    return res.send("No such user exists");
+
+  let tokenuser = decodedToken.userId
+  if (tokenuser != userId) return res.send({ status: false, msg: "You do not have Authoraisation" })
+  next()
+}
+
+module.exports.MW = mw
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+const mw=require("../auth")
 
 router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",mw.MW, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",mw.MW, userController.updateUser)
+
+router.delete("/users/:userId",mw.MW, userController.deleteddata)
 
 module.exports = router;
